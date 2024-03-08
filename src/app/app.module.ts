6272b91a00fe09e90d61c47e99a070db8f2b5ea6
tharinm <th4rinonline@gmail.com>
2024-03-08 23:15:42 +0530

    project branch created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
-import { SuccessAlertComponent } from './success-alert/success-alert.component';
-import { WarningAlertComponent } from './warning-alert/warning-alert.component';
-import { FormsModule } from '@angular/forms';
-import { SatabindingassignmentComponent } from './satabindingassignment/satabindingassignment.component';
-import { Assignment3Component } from './assignment3/assignment3.component';  // Import FormsModule here
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    ServersComponent,
-    SuccessAlertComponent,
-    WarningAlertComponent,
-    SatabindingassignmentComponent,
-    Assignment3Component,
+ 
   ],
-  imports: [BrowserModule, FormsModule],  // Add FormsModule to the imports array
+  imports: [BrowserModule, ],  // Add FormsModule to the imports array
   providers: [],
   bootstrap: [AppComponent],
 })
