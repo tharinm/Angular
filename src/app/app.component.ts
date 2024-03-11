c60e9a4265d54bbd837c8c4a637fef69f2e5f11d
tharinm <th4rinonline@gmail.com>
2024-03-11 22:54:52 +0530

    adding custom properties

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,12 +5,22 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   // styleUrl: './app.component.css'
   styles: [
-    `h3{
-      color: #ff006a;
-    }`
-  ]
+    `
+      h3 {
+        color: #ff006a;
+      }
+    `,
+  ],
 })
 export class AppComponent {
   title = 'ANGULAR TO DEEP HDKDN';
-  name="Tharindu"
+  name = 'Tharindu';
+
+  serverElement = [
+    {
+      type: 'Server',
+      name: 'Testserver',
+      content: 'Just a test !',
+    },
+  ];
 }
--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrl: './server-element.component.css'
+  styleUrl: './server-element.component.css',
 })
 export class ServerElementComponent {
+  @Input() element: { type: string; name: string; content: string };
 
+  constructor() {}
 }
